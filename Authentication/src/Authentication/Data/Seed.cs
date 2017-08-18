e827797b3842ccde7c4e58b84c6b5a8f26b2bcd8
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-08-18 09:37:52 +0700

    Init

---
+++ Authentication/src/Authentication/Data/Seed.cs
@@ -0,0 +1,34 @@
+﻿using Authentication.Models;
+using Authentication.Models.AccountViewModels;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.Data
+{
+    public class Seed
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public Seed(
+           UserManager<ApplicationUser> userManager
+         )
+        {
+            _userManager = userManager;
+        }
+        
+        public void Initialize(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+            if(context.Users.Any())
+            {
+                return;
+            }
+            var users = new ApplicationUser();
+            users.UserName = "Admin";
+            users.Email = "";
+            
+        }
+    }
+}
---
+++ Authentication/src/Authentication/Models/AccountViewModels/UserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.Models.AccountViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Code { get; set; }
+        public string Role { get; set; }
+        public List<string> UserRole { get; set; }
+        public List<IdentityRole> Roles { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+}
